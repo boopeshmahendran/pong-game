7feae56da507dfd075c0a16c1ebc60bb96009033
Boopesh <boopeshmahendran@gmail.com>
2016-10-14 00:33:06 +0530

    Changed color and styling of font in gameStart Screen

--- js/gameStart.js
+++ js/gameStart.js
@@ -7,11 +7,11 @@ var gameStart = {
 	create: function() {
 		this.stage.backgroundColor = '#e74c3c';
 		var title = this.add.text(this.game.world.width * 0.5,
-								 this.game.world.height * 0.5, "Pong Game\nClick to Start",
-								 {font: "50px Arial", fill: "#2c3e50"});
+								 this.game.world.height * 0.5, "PONG GAME\nCLICK TO START",
+								 {font: "50px Times New Roman", fill: "#212121"});
 		var helpText = this.add.text(this.game.world.width * 0.5,
-								 this.game.world.height * 0.8, "Use Up and Down to move Player 1\nUse W and S to move Player 2",
-								 {font: "30px Arial", fill: "#7f8c8d"});
+								 this.game.world.height * 0.8, "USE UP AND DOWN TO MOVE PLAYER 1\nUSE W AND S TO MOVE PLAYER 2",
+								 {font: "30px Tahoma", fill: "#4E342E"});
 		title.anchor.set(0.5);
 		helpText.anchor.set(0.5)
 		this.game.input.onDown.add(this.clickStart, this);
