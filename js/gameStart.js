fe11a4586c4607078a237593e6feb0879fd589f6
Boopesh <boopeshmahendran@gmail.com>
2016-10-13 23:52:37 +0530

    Added helptext for controls

--- js/gameStart.js
+++ js/gameStart.js
@@ -9,7 +9,11 @@ var gameStart = {
 		var title = this.add.text(this.game.world.width * 0.5,
 								 this.game.world.height * 0.5, "Pong Game\nClick to Start",
 								 {font: "50px Arial", fill: "#2c3e50"});
+		var helpText = this.add.text(this.game.world.width * 0.5,
+								 this.game.world.height * 0.8, "Use Up and Down to move Player 1\nUse W and S to move Player 2",
+								 {font: "30px Arial", fill: "#7f8c8d"});
 		title.anchor.set(0.5);
+		helpText.anchor.set(0.5)
 		this.game.input.onDown.add(this.clickStart, this);
 	},
 	update: function() {
